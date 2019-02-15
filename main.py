16e52c547c3ac08a11d4498379c41f8cfb80e8ad
Chigozirim Ogbogu <cogbogu@plexada-si.com>
2019-02-15 17:07:30 +0100

    added file via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+"""Code to submit as first assignment 
+In order to complete the assignment on variables as well as output the values of the variable""" 
+
+#Declare my variable here
+Artist = "Olamide"
+Genre = "Afro-pop"
+Song = "Woske"
+DurationInSeconds = 230
+ReleaseYear = 2019
+RecordLabel = "YNBL"
+ProducedBy = "KillerTunes"
+
+
+#Output Variable contents here
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+print(ReleaseYear)
+print(RecordLabel)
+print(ProducedBy)
