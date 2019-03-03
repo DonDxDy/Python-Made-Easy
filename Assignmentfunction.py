02728cbeb7a3757417b3cd4cbec4388f991ea221
Chigozirim Ogbogu <cogbogu@plexada-si.com>
2019-03-03 08:39:40 -0800

    Homework for #2 Functions

---
+++ Assignmentfunction.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  3 17:21:01 2019
+
+@author: Chigozirim
+"""
+
+"""Code to submit as Second assignment 
+In order to complete the assignment on functions """
+
+# Define function artist and return a value
+def artist():
+    return "Olamide" 
+
+# Define function genre and return a value
+def genre():
+    return "Afro-pop"
+
+# Define release year and return a value
+def releaseYear():
+    return 2019
+
+# Validate if this song has ever been released and  return a boolean
+def releasedBefore():
+    return False
+
+print("Artist: ", artist())
+
+print("Genre: ", genre())
+
+print("Album Year: ", releaseYear())
+
+print("Has This song ever been released before now?: ", releasedBefore())
+
+
+
